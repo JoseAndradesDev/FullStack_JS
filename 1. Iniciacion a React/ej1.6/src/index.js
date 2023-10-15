b305350891c0016ebea8895a65efb2f047b1952e
JoseAndradesDev <joseandradespl@gmail.com>
2023-10-15 05:00:21 +0200

    anadir comentarios

--- 1. Iniciacion a React/ej1.6/src/index.js
+++ 1. Iniciacion a React/ej1.6/src/index.js
@@ -2,19 +2,16 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  // save clicks of each button to its own state
 
-  const [clicks, setClicks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  //Clicks es el arrary como objeto js de los estados, setClick el disparador de renderizado
+  const [clicks, setClicks] = useState({good: 0, neutral: 0, bad: 0,});
 
+
+  //Setters que actualizan solo el valor correspondiente 
   const setGood = () => setClicks({ ...clicks, good: clicks.good + 1 });
-
   const setNeutral = () => setClicks({ ...clicks, neutral: clicks.neutral + 1 });
-
   const setBad = () => setClicks({ ...clicks, bad: clicks.bad + 1 });
+
 
   //Llamar a los componentes
   return (
@@ -32,19 +29,18 @@ const App = () => {
   );
 };
 
+
 //Creacion de componentes
 const Header = ({ titulo }) => <h1>{titulo}</h1>;
 
+//Comp. para botones que define su onclick
 const Boton = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Statistic = ({ text, value }) => (
-  <tr>
-    <td>
-      {text} {value}{" "}
-    </td>
-  </tr>
-);
+//Salida en pantalla de las estadisticas
+const Statistic = ({ text, value }) => <p>{text}{":"}{value}</p>;
 
+
+//Calcula el total y llama a cada registro de estadistica
 const Statistics = ({ clicks }) => {
   const total = clicks.good + clicks.neutral + clicks.bad;
 
@@ -61,4 +57,5 @@ const Statistics = ({ clicks }) => {
   );
 };
 
+//rendarizado de la app
 ReactDOM.render(<App />, document.getElementById("root"));
