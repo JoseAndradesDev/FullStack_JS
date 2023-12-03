c3d7144937424956f1db7b8583863578786642e8
JoseAndradesDev <joseandradespl@gmail.com>
2023-12-03 03:12:14 +0100

    regiones added

--- 2.-conectando-server/ej2.12-2.14-paises/src/components/Countrie.js
+++ 2.-conectando-server/ej2.12-2.14-paises/src/components/Countrie.js
@@ -6,13 +6,13 @@ const Countrie = ({countrie}) => {
   const  population = countrie.population;
   const  flag = countrie.flags.png;
   const  area = countrie.area;
-  const continent = countrie.continents; 
+  const subregion = countrie.subregion; 
 
   
   return (
     <div>
       <h1>{officialName}</h1>
-      <h3>{continent}</h3>
+      <h3>{subregion}</h3>
       <p>capital: {capital}</p>
       <p>population: {population} Millions habitans</p>
       <p>area: {area} Km2 </p>
