0448cec182a89357174409de29d6313570118b3e
JoseAndradesDev <joseandradespl@gmail.com>
2023-12-03 03:05:50 +0100

    V2 ej2.12 final

--- 2.-conectando-server/ej2.12-2.14-paises/src/components/Content.js
+++ 2.-conectando-server/ej2.12-2.14-paises/src/components/Content.js
@@ -2,18 +2,19 @@ import React from "react";
 import Countrie from "./Countrie";
 
 const Content = ({ countries }) => {
+
   if (countries.length > 10) {
     return <p>Demasiados paises, especifica mas</p>;
     
   } else if (countries.length <= 10 && countries.length > 1) {
-    /*return (
-      <ul>
-        {countries.map((countrie, i) => (
-          <li key={countrie[i].name}>{countrie[i].name}</li>
-        ))}
-      </ul>
-    );*/
-    
+     return (
+        <div>
+          {countries.map((country, i) =>
+            <p key={i}>{country.name.official}</p>
+          )}
+        </div>
+      )
+
   } else if (countries.length === 1) {
     return <Countrie countrie={countries[0]} />;
   }
--- 2.-conectando-server/ej2.12-2.14-paises/src/components/Countrie.js
+++ 2.-conectando-server/ej2.12-2.14-paises/src/components/Countrie.js
@@ -4,12 +4,20 @@ const Countrie = ({countrie}) => {
   const  officialName = countrie.name.official;
   const  capital = countrie.capital;
   const  population = countrie.population;
+  const  flag = countrie.flags.png;
+  const  area = countrie.area;
+  const continent = countrie.continents; 
+
   
   return (
     <div>
       <h1>{officialName}</h1>
+      <h3>{continent}</h3>
       <p>capital: {capital}</p>
       <p>population: {population} Millions habitans</p>
+      <p>area: {area} Km2 </p>
+    
+      <img src={flag} alt="Country flag"></img>
     </div>
   );
 };
