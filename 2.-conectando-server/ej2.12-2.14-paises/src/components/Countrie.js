e8aa5a6b12cb3750c437488dc53efdc07ea73808
JoseAndradesDev <joseandradespl@gmail.com>
2023-12-04 02:11:25 +0100

    resolucion ej2.14

--- 2.-conectando-server/ej2.12-2.14-paises/src/components/Countrie.js
+++ 2.-conectando-server/ej2.12-2.14-paises/src/components/Countrie.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Countrie = ({countrie}) => {
   const  officialName = countrie.name.official;
@@ -7,14 +8,29 @@ const Countrie = ({countrie}) => {
   const  flag = countrie.flags.png;
   const  area = countrie.area;
   const  languages = countrie.languages;
+  const continent = countrie.continents; 
   const subregion = countrie.subregion; 
 
+ const url = 'https://goweather.herokuapp.com/weather/'
 
+ const [weather, setWeather] = useState([])
+
+  axios.get(url+capital)
+  .then(response => {
+    const apiResponse = response.data;
+    setWeather(apiResponse)  
+  }).catch(error => {
+    console.log(error);
+  })
+  
+
+  //
   return (
-    <div>
+    <div> 
       <h1>{officialName}</h1>
-      <h3>{subregion}</h3>
-      <p>Capital: {capital}</p>
+      <img src={flag} alt="Country flag"></img>
+      <h2>{continent}</h2>
+      <p><strong>{subregion}</strong></p>
       <p>Area: {area} Km2 </p>
       <p>Population: {population} habitans</p>
       <p>Languages: </p>
@@ -23,7 +39,15 @@ const Countrie = ({countrie}) => {
           <li key={code}> {name}</li>
         ))}
       </ul>
-      <img src={flag} alt="Country flag"></img>
+      <p>Capital: {capital}</p>
+      <p>Weather in {capital} : </p>
+      <ul>
+        <li>Temperature: {weather.temperature}</li>
+        <li>Wind Speed: {weather.wind}</li>
+        <li>Descrption: {weather.description}</li>
+      </ul>
+      
+      
     </div>
   );
 };
